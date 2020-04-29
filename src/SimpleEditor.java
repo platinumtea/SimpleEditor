547ec7e1a1d70a56d8d44190eba77ae17131441e
platinumtea <platinumtea7@gmail.com>
2020-04-29 13:53:35 -0400

    Whitespace fix

--- src/SimpleEditor.java
+++ src/SimpleEditor.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class SimpleEditor {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("SimpleEditor");
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new SimpleEditorPanel());
 		frame.pack();
